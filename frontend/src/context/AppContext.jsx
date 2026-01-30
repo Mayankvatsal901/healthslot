c70e2959bd420226785a7649ed02603871d04b09
Mayankvatsal901 <mayankvatsal800@gmail.com>
2026-01-30 18:35:51 +0530

    Set default backend URL in AppContext

--- frontend/src/context/AppContext.jsx
+++ frontend/src/context/AppContext.jsx
@@ -9,7 +9,7 @@ export const AppContext=createContext()
 
 const AppContextProvider=(props)=>{
     const currencySymbol='$'
-    const backendUrl=import.meta.env.VITE_BACKEND_URL
+    const backendUrl=import.meta.env.VITE_BACKEND_URL||"https://healthslot-backend-3r09.onrender.com"
      const [doctors,setDoctor]=useState([])
      const [token,setToken]=useState(localStorage.getItem('token')?localStorage.getItem('token'):false)
      const [userData,setUserData]=useState(false)
